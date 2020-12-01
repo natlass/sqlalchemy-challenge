bf607aaccb921fe1241ba1a2b43f803c62cbf518
nlass <natlassen@gmail.com>
2020-11-30 23:49:34 -0500

    finished files

--- app.py
+++ app.py
@@ -45,37 +45,72 @@ def main():
 def precip():
     """Convert the query results to a dictionary using date as the key and prcp as the value"""
     """Return JSON representation of the dictionary"""
-    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
-    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    precip = session.query(measurement.date, measurement.prcp).\
-    filter(measurement.date >= year_ago).all()
-    
+    session = Session(engine)
+    results = session.query(measurement.date, measurement.prcp).all()
+    session.close()
+    prcp_list = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_list.append(prcp_dict)
+
+    return jsonify(prcp_list)
     
 @app.route("/api/v1.0/stations")
 def stations():
-    all_station = session.query(Station).all()
+    all_station = session.query(Station.name).all()
+    session.close()
     station_list = []
     for station in all_station:
-        stat_dict = {}
-        stat_dict["id"]=station.id
-        stat_dict["station"] = station.station
-        stat_dict["name"] = station.name
-        stat_dict["latitude"] = station.latitude
-        stat_dict["longitude"] = station.longitude
-        stat_dict["elevation"] = station.elevation
-        stat_dict.append(stat_dict)
+        station_list.append(station)
+    return jsonify(stations)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    
+    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    session.close()
+    print(last_date, year_ago)
+
+    active_station = session.query(measurement.station, func.count(measurement.station)).\
+        group_by(measurement.station).\
+        order_by(func.count(measurement.station).desc()).all()
+    session.close()
+    print(active_station[0][0])
+
+    session.query(measurement.date, measurement.tobs).\
+    filter(measurement.station == 'USC00519281').\
+    filter(measurement.date >= '2016-08-23').all()
+    session.close()
+
     
 @app.route("/api/v1.0/<start>")
 def start(start):
-    
+    session = Session(engine)
+
+    sel = [func.min(measurement.tobs), 
+        func.round(func.avg(measurement.tobs),1), 
+        func.max(measurement.tobs),]
+
+    start_temp = session.query(*sel).\
+        filter(measurement.date >= start).all()
+
+    return (jsonify(start_temp))
 
 @app.route("/api/v1.0/<start>/<end>")
 def end(start, end):
-    
+    session = Session(engine)
+
+    sel = [func.min(measurement.tobs), 
+        func.round(func.avg(measurement.tobs),1), 
+        func.max(measurement.tobs),]
+
+    end_temp = session.query(*sel).\
+        filter(measurement.date >= start).\
+        filter(measurement.date <= end).all()
+
+    return jsonify(end_temp)
 
 if __name__ == "__main__":
     app.run(debug = True)
